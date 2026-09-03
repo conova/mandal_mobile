1e20b72eb44562e7432d9a04b4c5fc07c18e9cfc
Tuguldur Bat-Erdene <tuguldurkanzen@gmail.com>
2026-09-03 17:09:45 +0800

    unneccessary imports

--- lib/screens/components/bond/bond_status_info_sheet.dart
+++ lib/screens/components/bond/bond_status_info_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mandal_capital/theme/app_text_styles.dart';
 import '../../../l10n/app_localizations.dart';
 import '../../../theme/extended_colors.dart';
 import '../../../widgets/custom_button.dart';
